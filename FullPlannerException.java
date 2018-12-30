55fe055fc2485ad5689cee04b15d4b61208c2243
vivi3968 <46255198+vivi3968@users.noreply.github.com>
2018-12-30 00:10:19 -0500

    Update FullPlannerException.java

--- FullPlannerException.java
+++ FullPlannerException.java
@@ -1,17 +1,8 @@
-/* Vivian Lam
- * ID: 111549991
- * vivian.lam@stonybrook.edu
- * Homework 1
- * CSE214, R11 (Reed Gantz)
- */ 
-
 /**
  * Exception for when Planner is full
  */
 public class FullPlannerException extends Exception {
-	/**
-	 * Constructor for FullPlannerException that takes no parameters
-	 */
+	
 	public FullPlannerException() {
 		super("Planner is full");
 	}
